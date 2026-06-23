dd6e806592c6d0c8dcfcddc2ab116c22d11caba6
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-06-23 19:53:43 +0800

    Wire-up orders UI fragment

--- FE/app/src/main/java/com/example/kantinapplication/OrdersFragmentActivity.kt
+++ FE/app/src/main/java/com/example/kantinapplication/OrdersFragmentActivity.kt
@@ -1,12 +1,12 @@
 package com.example.kantinapplication
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-class DashboardFragmentActivity : Fragment() {
+class OrdersFragmentActivity : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +16,7 @@ class DashboardFragmentActivity : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_orders, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
