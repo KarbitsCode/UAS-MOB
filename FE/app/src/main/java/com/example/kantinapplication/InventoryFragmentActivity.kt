0d5a4adb83d69aa364d29b74f012edc7c246d9a8
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-06-25 16:27:00 +0800

    Inventory UI fragment

--- FE/app/src/main/java/com/example/kantinapplication/InventoryFragmentActivity.kt
+++ FE/app/src/main/java/com/example/kantinapplication/InventoryFragmentActivity.kt
@@ -1,10 +1,14 @@
 package com.example.kantinapplication
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class InventoryFragmentActivity : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +27,30 @@ class InventoryFragmentActivity : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Start here
+        view.findViewById<FloatingActionButton>(R.id.addInventoryFloatingActionButton).setOnClickListener {
+            val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_add_edit_inventory, null)
+            val builder = AlertDialog.Builder(context).setView(dialog).create()
+            dialog.findViewById<TextView>(R.id.titleTextView6).text = "TAMBAH PRODUK BARU"
+            dialog.findViewById<Button>(R.id.inventorySaveActionButton).setOnClickListener {
+                // TODO: insert ke database dan tampilkan ke inventoryRecyclerView
+                // inventoryNameEditText, inventoryPriceEditText, inventoryStockEditText
 
+                builder.dismiss()
+            }
+            builder.show()
+        }
+
+        view.findViewById<Button>(R.id.editStockActionButton)?.setOnClickListener {
+            val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_add_edit_inventory, null)
+            val builder = AlertDialog.Builder(context).setView(dialog).create()
+            dialog.findViewById<TextView>(R.id.titleTextView6).text = "EDIT DATA PRODUK"
+            dialog.findViewById<Button>(R.id.inventorySaveActionButton).setOnClickListener {
+                // TODO: update ke database dan tampilkan ke inventoryRecyclerView
+                // inventoryNameEditText, inventoryPriceEditText, inventoryStockEditText
+
+                builder.dismiss()
+            }
+            builder.show()
+        }
     }
 }
