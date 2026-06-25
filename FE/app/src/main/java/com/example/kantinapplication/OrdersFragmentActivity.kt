c16d6cf867443e60fadd9b56cb29ac58c32d4593
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-06-25 14:48:57 +0800

    Add orders button

--- FE/app/src/main/java/com/example/kantinapplication/OrdersFragmentActivity.kt
+++ FE/app/src/main/java/com/example/kantinapplication/OrdersFragmentActivity.kt
@@ -1,10 +1,12 @@
 package com.example.kantinapplication
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class OrdersFragmentActivity : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,18 @@ class OrdersFragmentActivity : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Starts here
+        view.findViewById<FloatingActionButton>(R.id.addOrderFloatingActionButton).setOnClickListener {
+            val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_add_order, null)
+            val builder = AlertDialog.Builder(context)
+                .setView(dialog)
+                .create()
+            dialog.findViewById<View>(R.id.orderSaveActionButton).setOnClickListener {
+                // TODO: insert ke database dan tampilkan ke ordersRecyclerView
+                // orderIdEditText, orderDetailsEditText
 
+                builder.dismiss()
+            }
+            builder.show()
+        }
     }
 }
